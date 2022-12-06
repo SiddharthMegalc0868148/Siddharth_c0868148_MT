4816366b719713fe43c4e98c4377abf8988c4932
siddharth <Siddharthmegal911@gmail.com>
2022-12-05 20:14:42 -0500

    1

--- app/src/main/java/com/example/siddharth_c0868148_mt/MainActivity.java
+++ app/src/main/java/com/example/siddharth_c0868148_mt/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.siddharth_c0868148_mt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String TAG = "Android";
+    Spinner spinner;
+    Button ViewDetails;
+    RadioGroup RG;
+    CheckBox C1;
+    CheckBox C2;
+    CheckBox C3;
+    TextView textView;
+    TextView textView2;
+    TextView textView3;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.id.
     }
 }
