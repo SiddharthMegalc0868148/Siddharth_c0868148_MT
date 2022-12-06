fb2d8d3877e4b10c085f441594d775830ee68cec
siddharth <Siddharthmegal911@gmail.com>
2022-12-05 20:53:54 -0500

    1

--- app/src/main/java/com/example/siddharth_c0868148_mt/MainActivity.java
+++ app/src/main/java/com/example/siddharth_c0868148_mt/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.siddharth_c0868148_mt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -15,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     String cars = "Cars";
     Spinner spinner;
+    SeekBar seekBar;
     Button ViewDetails;
     RadioGroup RG;
     CheckBox C1;
@@ -22,15 +26,25 @@ public class MainActivity extends AppCompatActivity {
     CheckBox C3;
     TextView textView;
     TextView textView2;
-    TextView textView3;
+    TextView textView4;
     EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        seekBar = findViewById(R.id.seekBar);
         spinner = findViewById(R.id.spinner1);
+        ViewDetails = findViewById(R.id.Btn);
+        RG = findViewById(R.id.radioGroup);
+        C1 = findViewById(R.id.checkBox);
+        C2 = findViewById(R.id.checkBox2);
+        C3 = findViewById(R.id.checkBox3);
+        textView = findViewById(R.id.textView);
+        textView2 = findViewById(R.id.textView2);
+        textView4 = findViewById(R.id.textView4);
+        editText = findViewById(R.id.editText);
+
 
     }
 }
